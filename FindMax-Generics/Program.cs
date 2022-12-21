4794559c682e68f61a3ee20f3b2017638fc1c6fc
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-21 15:44:11 +0530

    [Akshay]-Find Maximum Integer

---
+++ FindMax-Generics/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace FindMax_Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int Max = FindMax.FindIntMax(6, 3, 12);
+            Console.WriteLine(Max);
+        }
+    }
+    
+}
