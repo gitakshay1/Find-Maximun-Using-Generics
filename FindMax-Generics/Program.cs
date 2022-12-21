62e320f38cebf40df83477cdcc51dbbc336cc47c
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-21 15:49:24 +0530

    [Akshay]-1 St Position

--- FindMax-Generics/Program.cs
+++ FindMax-Generics/Program.cs
@@ -5,7 +5,7 @@ namespace FindMax_Generics
     {
         static void Main(string[] args)
         {
-            int Max = FindMax.FindIntMax(6, 3, 12);
+            int Max = FindMax.FindIntMax(3, 2, 1);
             Console.WriteLine(Max);
         }
     }
